1b0e244d158f16c76d0721bbbe44dbb38779ae46
CharleneMills <charleneskater@gmail.com>
2020-06-04 17:32:43 -0600

    DONE!

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const expressSession = require('express-session');
 const massive = require('massive');
-const authCtrl = require('./controllers/authController')
+const authCtrl = require('./controllers/authController');
+const treasureCtrl = require('./controllers/treasureController');
+const auth = require('./middleware/authMiddleware');
 
 
 const app = express();
@@ -35,6 +37,12 @@ app.use(
 app.post('/auth/register', authCtrl.register)
 app.post('/auth/login', authCtrl.login)
 app.get('/auth/logout', authCtrl.logout)
+
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
+app.get('/api/treasure/user', treasureCtrl.getUserTreasure)
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure)
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure)
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure);
 
 
 
